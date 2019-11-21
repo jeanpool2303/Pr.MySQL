6b3c80f9a71695f355bca65fe6dbe7a9e0951dbf
jean pool palomino <jean.palomino@vallegrande.edu.pe>
2019-11-21 00:42:37 -0500

    tienda 2.0

--- Tienda informatica/tienda.sql
+++ Tienda informatica/tienda.sql
@@ -76,3 +76,69 @@ select nombre, precio
 from producto 
 order by nombre asc,
 precio desc;
+/*16 Devuelve una lista con las 5 primeras filas de la tabla fabricante.*/
+select * from fabricante order by codigo limit 5 ;
+/*17 Devuelve una lista con 2 filas a partir de la cuarta fila de la tabla fabricante. La cuarta fila también se debe incluir en la respuesta.*/
+select * from fabricante order by codigo asc limit 3,6;
+/*18 Lista el nombre y el precio del producto más barato. (Utilice solamente las cláusulas 
+ORDER BY y LIMIT*/
+select nombre, precio from producto order by precio limit 100;
+/*19 Lista el nombre y el precio del producto más caro. (Utilice solamente las cláusulas
+ORDER BY y LIMIT)*/
+select nombre, precio from producto order by precio desc limit 100;
+/*20 Lista el nombre de todos los productos del fabricante cuyo código de fabricante es igual a 2.*/
+select codigo_fabricante, nombre from producto where codigo_fabricante = 2;
+/*21 Lista el nombre de los productos que tienen un precio menor o igual a 120€.*/
+select nombre, precio
+from producto
+where precio<=120;
+/*22 Lista el nombre de los productos que tienen un precio mayor o igual a 400€.*/
+select nombre, precio
+from producto
+where precio>=400;
+ /*23 Lista el nombre de los productos que no tienen un precio mayor o igual a 400€.*/
+select nombre, precio
+from producto
+where precio<400;
+/*24 Lista todos los productos que tengan un precio entre 80€ y 300€. Sin utilizar el operador BETWEEN*/
+select nombre, precio
+from producto
+where precio>80 and precio<300;
+/*25 Lista todos los productos que tengan un precio entre 60€ y 200€. Utilizando el operador BETWEEN.*/
+select nombre, precio 
+from producto 
+where precio between 60 and 200;
+/*26 Lista todos los productos que tengan un precio mayor que 200€ y que el código de fabricante sea igual a 6.*/
+select codigo_fabricante, nombre, precio 
+from producto 
+where codigo_fabricante = 2 and precio > 200;
+ /*27 Lista todos los productos donde el código de fabricante sea 1, 3 o 5. Sin utilizar el operador IN.*/
+ select * from producto 
+ where codigo % 2 != 0
+ order by codigo limit 0,3;
+ /*28 Lista todos los productos donde el código de fabricante sea 1, 3 o 5. Utilizando el operador IN*/
+ select * from producto
+ where codigo in(1,5,3);
+ /*29 Lista el nombre y el precio de los productos en céntimos (Habrá que multiplicar por 100 el valor del precio). Cree un alias para la columna que contiene el precio que se llame céntimos.*/
+ select nombre, precio*100 as centimos 
+ from producto;
+ /*30 Lista los nombres de los fabricantes cuyo nombre empiece por la letra S*/
+ select * from fabricante
+ where nombre like 'S%';
+ /*31 Lista los nombres de los fabricantes cuyo nombre termine por la vocal e.*/
+ select * from fabricante
+ where nombre like '%e';
+ /*32 Lista los nombres de los fabricantes cuyo nombre contenga el carácter w.*/
+ SELECT nombre FROM fabricante WHERE nombre LIKE "%w%";
+ /*33 Lista los nombres de los fabricantes cuyo nombre sea de 4 caracteres.*/
+ select  nombre from fabricante
+ where length(nombre)=4;
+ /*34 Devuelve una lista con el nombre de todos los productos que contienen la cadena Portátil en el nombre.*/
+ select nombre, precio  FROM PRODUCTO where nombre LIKE "%Portatil%";
+ /*35 Devuelve una lista con el nombre de todos los productos que contienen la cadena Monitor en el nombre y tienen un precio inferior a 215 €.*/
+ select * from producto 
+ where nombre like 'Monito%' and precio<215;
+ /*36 Lista el nombre y el precio de todos los productos que tengan un precio mayor o igual a 180€. Ordene el resultado en primer lugar por el precio (en orden descendente) y en segundo lugar por el nombre (en orden ascendente).*/
+ SELECT * FROM producto
+WHERE precio >= 180
+ORDER BY precio ASC, precio DESC;
